233f3922c5ebd0f081701f39d1b668b7f09e1cf5
Joseph Rork <jrork1@gmail.com>
2021-02-06 23:23:26 -0500

    Added modes

--- DrumLightEEPROMread.ino
+++ DrumLightEEPROMread.ino
@@ -4,20 +4,32 @@
 
 #include <EEPROM.h>
 #include "drums.h"
+#include "modes.h"
 
-drumLight myDrumLight = {Bass5, 62223, 100, 50, 35, 1, 2};  
+struct drumLight {
+  drumID drumId;
+  uint32_t color;
+  uint8_t brightness;
+  uint32_t threshold;
+  uint8_t delayValue;
+  modeID triggerMode;
+  uint8_t pixelCount;
+};
+
+drumLight myDrumLight = {Bass5, 62223, 100, 50, 35, offMode, 2};  
 
 void setup()
 {  
   
   Serial.begin(74880); //Serial communication to display data
-  Serial.printf("Drum id before EEPROM read: %d\n", myDrumLight.drumId);
+  Serial.printf("Drum id before EEPROM read: %s\n", DrumText[myDrumLight.drumId]);
   Serial.printf("Color value before EEPROM read: %d\n", myDrumLight.color);
   Serial.printf("Brightness value before EEPROM read: %i\n", myDrumLight.brightness);
   Serial.printf("Threshold value before EEPROM read: %i\n", myDrumLight.threshold);
   Serial.printf("Delay time value before EEPROM read: %i\n", myDrumLight.delayValue);
-  Serial.printf("Trigger Mode value before EEPROM read: %i\n", myDrumLight.triggerMode);
+  Serial.printf("Trigger Mode value before EEPROM read: %s\n", ModeText[myDrumLight.triggerMode]);
   Serial.printf("Pixel count value before EEPROM read: %i\n", myDrumLight.pixelCount);
+  Serial.println();
 
   char* myDrumLightBytes = reinterpret_cast<char*>(&myDrumLight);
   const uint32_t myDrumLightSize = sizeof(myDrumLight);
@@ -28,12 +40,12 @@ void setup()
   }
   memcpy(&myDrumLight, myDrumLightBytes, sizeof(drumLight));
   
-  Serial.printf("Drum id after EEPROM read: %d\n", myDrumLight.drumId);
+  Serial.printf("Drum id after EEPROM read: %s\n", DrumText[myDrumLight.drumId]);
   Serial.printf("Color value after EEPROM read: %d\n", myDrumLight.color);
   Serial.printf("Brightness value after EEPROM read: %i\n", myDrumLight.brightness);
   Serial.printf("Threshold value after EEPROM read: %i\n", myDrumLight.threshold);
   Serial.printf("Delay time value after EEPROM read: %i\n", myDrumLight.delayValue);
-  Serial.printf("Trigger Mode value after EEPROM read: %i\n", myDrumLight.triggerMode);
+  Serial.printf("Trigger Mode value after EEPROM read: %s\n", ModeText[myDrumLight.triggerMode]);
   Serial.printf("Pixel count value after EEPROM read: %i\n", myDrumLight.pixelCount);
 
 }
